310bcdf8d07b902b9e3ed21f2d47f0349f6c025a
Peter Crowther <petercrowther90@hotmail.com>
2017-12-06 16:15:05 +0000

    4 tests passed

--- array_tasks.js
+++ array_tasks.js
@@ -2,19 +2,29 @@ var arrayTasks = {
 
 	concat: function (arr1, arr2) {
 		return arr1.concat(arr2);
-	}
+	},
 
 	insertAt: function (arr, itemToAdd, index) {
+			arr.splice(index, 0, itemToAdd);
+			return arr;
+	},
 
-	}
+	square: function (arr) {
+		let newArr = [];
+		for(item of arr){
+			result = item * item
+			newArr.push(result);
+		}
+		return newArr;
+	},
 
-	// square: function (arr) {
-
-	// },
-
-	// sum: function (arr) {
-
-	// },
+	sum: function (arr) {
+		total = 0
+		for(item of arr){
+			total += item;
+		}
+		return total;
+	},
 
 	// findDuplicates: function (arr) {
 
--- tests.js
+++ tests.js
@@ -10,17 +10,17 @@ describe('Array tasks', function () {
 		assert.deepStrictEqual(arrayTasks.concat(arr1, arr2), expectation)
 	})
 
-	xit('should insert an item in an array at any index position', function () {
+	it('should insert an item in an array at any index position', function () {
 		var arr = [1, 2, 4]
 		assert.deepStrictEqual(arrayTasks.insertAt(arr, 3, 2), [1, 2, 3, 4])
 	})
 
-	xit('should square all values in an array, returning a new array', function () {
+	it('should square all values in an array, returning a new array', function () {
 		var arr = [1, 2, 3, 4, 5]
 		assert.deepStrictEqual(arrayTasks.square(arr), [1, 4, 9, 16, 25])
 	})
 
-	xit('should calculate the sum of all values in an array', function () {
+	it('should calculate the sum of all values in an array', function () {
 		var arr = [1, 2, 3, 4, 5]
 		assert.strictEqual(arrayTasks.sum(arr), 15)
 	})
